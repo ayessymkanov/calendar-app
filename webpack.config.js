1fba7abab703ea8273701ccf95003f196eb47267
ayessymkanov <arman.yessymkanov@gmail.com>
2018-10-01 14:27:05 +0600

    setup environment

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: '/node_modules/',
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ]
+}
